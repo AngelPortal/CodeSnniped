ded8b558288383db10896c203b3e556ea4c44ef6
AngelPortal <angel.portal@serpro.gov.br>
2016-05-04 15:59:35 -0300

    Primera Versão - Snniped para Windows

---
+++ TestClass.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+				
+namespace CodeSnnied
+{
+	/// <summary>
+	/// Write the summary for the test class here.
+	/// </summary>
+	[TestFixture]
+	public class TestClass
+	{
+		/// <summary>
+		/// Write your setup story here.
+		/// </summary>
+		[SetUp] 
+		public void Setup()
+		{
+		
+		}
+		
+		/// <summary>
+		/// Write your teardown story here.
+		/// </summary>
+		[TearDown] 
+		public void Dispose()
+		{
+
+		}
+
+		/// <summary>
+		/// Write your test story here.
+		/// </summary>
+		/// <remarks>
+		/// - Desired Result:	DesiredResult
+		/// - Coordinator:      When
+		/// - Conditions:       Conditions
+		/// </remarks>
+		[Test]
+		public void DesiredResult_When_Conditions()
+		{
+			
+		}
+	}
+}
